ed07a614cf59080ab13fec4acddb1893d625febd
Pal-Lord <amroabouhachem@hotmail.com>
2020-05-05 15:47:59 +0300

    TEST

--- src/App.js
+++ src/App.js
@@ -38,6 +38,13 @@ export default function ButtonAppBar() {
           <Button color="inherit">Sign-up</Button>
         </Toolbar>
       </AppBar>
+      <div>
+      <h1>Welcome !</h1>
+      <br></br>
+      If you are here, good chances are you’re creative, passionate and clever.
+      Code for Palestine was born in 2015 out of a desire to teach coding and entrepreneurial skills to some of the brightest high school students throughout the West Bank and Gaza Strip.  The organization funding the initiative, the PalTel Group Foundation [PGF], recognized that there weren’t enough students graduating from Palestinian universities in STEM fields, and those that were graduating lacked some of the critical skills needed to succeed in the private sector.  Most importantly, PGF saw an opportunity for a tech-driven startup scene to help tackle some of Palestine’s problems and realized that a culture of entrepreneurship needed to be bred in order for a startup culture to blossom. Code for Palestine is a three-year-long program, beginning the summer before 10th grade and concluding after 12th grade.  Each year has many in-person sessions but is kicked off by a two-week camp held during the summer.  Volunteer undergraduate instructors from Stanford University have served as the primary teachers during the two-week summer camp.
+
+      </div>
     </div>
   );
 }
