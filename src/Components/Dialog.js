8d9a903cbae731650438434dd20d4c1b20a4ba33
Naim-Qawasmeh <49104740+Naim-Qawasmeh@users.noreply.github.com>
2020-05-24 17:15:56 +0300

    Add files via upload

---
+++ src/Components/Dialog.js
@@ -0,0 +1,85 @@
+import React, { Fragment, Component } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { names } from "../store";
+import User from "./CFP User/App";
+import ReactDOM from 'react-dom';
+
+var x=0
+class Hello extends Component {
+  state = {
+    open: false,
+    chao: "",
+  };
+  click = () => {
+    this.setState({
+      open: !this.state.open,
+    });
+  };
+  handleChange = (name) => ({ target: { value } }) => {
+      this.setState({
+          chao: value,
+        });
+    };
+    login = (props) => {
+        names.map((name) => {
+            if (name === this.state.chao) {
+                ReactDOM.render(
+                    <React.StrictMode>
+                      <User />
+                    </React.StrictMode>,
+                    document.getElementById('root')
+                  );
+    
+          
+    }
+    });
+  };
+  
+  render() {
+   
+      return (
+          <Fragment>
+        <Button variant="contained" color="primary" onClick={this.click}>
+          Login{" "}
+        </Button>{" "}
+        <Dialog
+          aria-labelledby="form-dialog-title"
+          open={this.state.open}
+          onClose={this.click}
+          >
+          <DialogTitle id="form-dialog-title">Login</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter your name here to get in
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Your Name"
+              type="text"
+              onChange={this.handleChange("name")}
+              fullWidth
+              />
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.login}>
+              Login
+            </Button>
+            
+          </DialogActions>
+        </Dialog>
+        
+      </Fragment>
+    );
+ }  
+
+}
+export default Hello;
+
---
+++ src/store.js
@@ -0,0 +1,20 @@
+import React from "react";
+export const names = ["Naeam", "Jack"];
+export const topic = [
+  "Educational Materiel :",
+  ,
+  "Design Thinking :",
+  ,
+  "1. Empathising",
+  ,
+  "2-Defining ",
+  ,
+  "3- Ideating:",
+];
+export const explane = [
+  "Classes throughout the program are divided into two, computer science (CS) classes and design thinking classes (DT). These two together allow students to strategically analyze problems, come with creative solutions and utilize technology and programming to implement these solutions.",
+  " DT refers to the cognitive, strategic and practical processes by which design concepts (proposals for new products, buildings, machines, etc.) are developed.The core aim of DT is to understand people’s needs and creatively offer solutions to meet these needs. Accordingly, students are exposed to 5 non-linear steps that make up the core of design thinking.",
+  "Firstly, students are taught how to better and understand problems, people’s needs and challenges involved. This is viable through teaching students how to observe, engage and empathize with people to understand their experiences and motivations, as well as immersing ",
+  "Then students are taught how to analyze and synthesise gathered information in order to define main problems and challenges. This process should end a human-centred problem statement that describes the problem.",
+  "In this stage, students should generate as many ideas as possible should be diverse and should not be restricted to a certain methodology or approach. At the end of this process, students are taught how to filter these ideas through certain measures, and so they should leave this stage with ideas ready for prototyping.",
+];
