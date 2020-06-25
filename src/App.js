04866f5696a3fcb13da872d03cdd9c039ab16d03
Pal-Lord <amroabouhachem@gmail.com>
2020-06-25 16:51:27 +0300

    77

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ export default function ButtonAppBar() {
       instructors from Stanford University have served as the primary
       teachers during the two-week summer camp.
       </h4>
-      <h2>I am testing</h2>
+      <h2>I am testing for fun</h2>
       </div>
     </div>
   );
