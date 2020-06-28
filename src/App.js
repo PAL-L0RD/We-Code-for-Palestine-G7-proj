4dfd252e0fceb2c2b449bb3cd68e245797717c0f
Amro Abou-Hachem <amroabouhachem@gmail.com>
2020-06-28 17:39:07 +0300

    7

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
 import { AppBar } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
-
+import Bar1 from './Components/MainPage';
 const useStyles = makeStyles((theme) => ({
   wrapper: {
     display: 'block',
@@ -43,24 +43,7 @@ export default function Main() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            <img alt={''} src={require('./Components/Pictures/logo1.jpg')} />
-            Code For Palestine!
-          </Typography>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Sign-up</Button>
-        </Toolbar>
-      </AppBar>
+      <Bar1 />
       <div className={classes.wrapper}>
         <br></br>
         <div
