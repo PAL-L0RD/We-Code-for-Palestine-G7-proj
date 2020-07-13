b08f80b2ba4709a5f4fb20d33743e0fa88f6e192
Amro Abou-Hachem <amroabouhachem@gmail.com>
2020-07-13 12:02:19 +0300

    456

--- src/Components/Main-Project-Screens/Amro_Footer/Amro_Footer.jsx
+++ src/Components/Main-Project-Screens/Amro_Footer/Amro_Footer.jsx
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import Divider from '@material-ui/core/Divider';
 import ReactDOM from 'react-dom';
 import { Grid } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+import Footeritems from './footeritems';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import './Amro_Footer.css';
@@ -10,33 +12,50 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/Inbox';
 import Im1 from './CFP-removebg-preview.png';
+import Paltel from './Paltel-removebg-preview.png';
 
 function Amro_footer() {
   return (
-    <div>
-      <Fragment>
+    <div className="main">
+      <footer>
         <Divider className="divider" />
-        <Grid className="mid" container direction="row" xs={12}>
-          <Grid item xs={3} className="mid">
-            <div className="mid">
-              <img className="img" src={Im1} />
-            </div>
+        <Grid container direction="row" xs={12}>
+          <Grid item xs={0} sm={4}>
+            <Box display={{ xs: 'none', sm: 'block' }}>
+              <div className="image">
+                <img className="img" src={Im1} />
+              </div>
+            </Box>
           </Grid>
-          <Grid item xs={4}>
-            <List component="nav" aria-label="secondary mailbox folder">
-              <ListItem button>
+          <Grid item sm={4} xs={12}>
+            {/* <List component="nav" aria-label="footer-0" className="mid">
+              <ListItem button className="mid">
                 <Link to="/about-us"> Feedback</Link>
               </ListItem>
-              <ListItem button>
+              <ListItem className="mid" button>
                 <Link to="/about-us"> Contact-us</Link>
               </ListItem>
-            </List>
+              <ListItem className="mid" button>
+                <Link to="/about-us"> Admission Dates</Link>
+              </ListItem>
+              <ListItem className="mid" button>
+                <Link to="/about-us">
+                  <Typography className="footeritemas" varient="h2">
+                    Pictures and videos
+                  </Typography>
+                </Link>
+              </ListItem>
+            </List> */}
+            <Footeritems />
           </Grid>
-          <Grid item xs={4}>
-            <h1>Sponsers:</h1>
+          <Grid item xs={12} sm={4} className="mid">
+            <Typography className="Sponsor" varient="h2">
+              {'Sponser:'}
+            </Typography>
+            <img className="image" src={Paltel} />
           </Grid>
         </Grid>
-      </Fragment>
+      </footer>
     </div>
   );
 }
---
+++ src/Components/Main-Project-Screens/Amro_Footer/footeritems.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+import { red, green, lightGreen } from '@material-ui/core/colors';
+import EventIcon from '@material-ui/icons/Event';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: lightGreen,
+  },
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+export default function SimpleList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <List component="nav" aria-label="main mailbox folders">
+        <Link to="about-us">
+          <ListItem button>
+            <ListItemIcon>
+              <ContactPhoneIcon />
+            </ListItemIcon>
+
+            <ListItemText primary="Contact Us"></ListItemText>
+          </ListItem>
+        </Link>
+        <Link className="talk" to="about-us">
+          <ListItem button>
+            <ListItemIcon>
+              <ContactSupportIcon />
+            </ListItemIcon>
+
+            <ListItemText primary="Questions"></ListItemText>
+          </ListItem>
+        </Link>
+        <Link className="talk" to="about-us">
+          <ListItem button>
+            <ListItemIcon>
+              <EventIcon />
+            </ListItemIcon>
+
+            <ListItemText primary="Admission Dates"></ListItemText>
+          </ListItem>
+        </Link>
+      </List>
+    </div>
+  );
+}
