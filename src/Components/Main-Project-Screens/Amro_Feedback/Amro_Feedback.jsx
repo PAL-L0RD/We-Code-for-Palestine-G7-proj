aa88e1cabb24ce8452a26e92fb5c1f5153b0d66d
Amro Abou-Hachem <amroabouhachem@gmail.com>
2020-07-14 11:51:34 +0300

    64

--- src/Components/Main-Project-Screens/Amro_Feedback/Amro_Feedback.jsx
+++ src/Components/Main-Project-Screens/Amro_Feedback/Amro_Feedback.jsx
@@ -1,12 +1,18 @@
 import React, { Fragment, useState } from 'react';
 import { Grid } from '@material-ui/core';
 import './Amro_Previous_styles.css';
+import db from '../../../firebase';
 
 const Amro_feedback = (props) => {
   const [name, setName] = useState('');
   const [feedback, setFeedback] = useState('');
   const now = new Date();
-  const submit = (evnet, props) => {};
+  const submit = () => {
+    db.collection('Feedback').doc(now).set({
+      fullname: name,
+      feedback: feedback,
+    });
+  };
   return (
     <Fragment>
       <form onSubmit={submit}>
